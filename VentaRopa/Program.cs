3d626750b4c25ecf22f7ec48b4c50ed8c955a7f2
María Fernanda Quesada Vega <maria.quesada@ucrso.info>
2024-06-22 18:51:46 -0600

    Subido al hosting

--- VentaRopa/Program.cs
+++ VentaRopa/Program.cs
@@ -25,7 +25,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
-
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
